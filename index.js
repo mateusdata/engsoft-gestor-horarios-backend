36b66eeace8aa6af92c94c44250d53b08e3776f3
mateusdata <mateuspele2015@gmail.com>
2023-10-07 04:45:56 -0300

    Change env

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ app.use(express.json());
 app.use(cors());
 require('dotenv').config();
 
-
 //Os commit serão feito na feature/newmaster
 const db = mysql.createConnection({
     host: process.env.DB_HOST,
@@ -23,51 +22,9 @@ db.connect((err) => {
     console.log('Conexão bem-sucedida ao banco de dados');
   }
 });
-
-
-app.get('/criar-tabela-aulas', function (req, res) {
-    const sql = `
-      CREATE TABLE IF NOT EXISTS aulas (
-        id INT AUTO_INCREMENT PRIMARY KEY,
-        nome_professor VARCHAR(255) NOT NULL,
-        materia VARCHAR(255) NOT NULL,
-        disponibilidade_em_dias INT NOT NULL
-      )
-    `;
-  
-    db.query(sql, (err, results) => {
-      if (err) {
-        console.error('Erro ao criar a tabela "aulas":', err);
-        res.status(500).json({ error: 'Erro ao criar a tabela "aulas"' });
-      } else {
-        res.json({ message: 'Tabela "aulas" criada com sucesso' });
-      }
-    });
-  });
-
-  app.get('/inserir-aula', function (req, res) {
-    const nomeProfessor = 'Professor Teste';
-    const materia = 'Matéria de Teste';
-    const disponibilidadeEmDias = 5; 
-  
-    const sql = 'INSERT INTO aulas (nome_professor, materia, disponibilidade_em_dias) VALUES (?, ?, ?)';
-  
- 
-    const values = [nomeProfessor, materia, disponibilidadeEmDias];
-  
-    db.query(sql, values, (err, results) => {
-      if (err) {
-        console.error('Erro ao inserir aula:', err);
-        res.status(500).json({ error: 'Erro ao inserir aula' });
-      } else {
-        res.json({ message: 'Aula inserida com sucesso' });
-      }
-    });
-  });
-  
   
 app.get('/users', function (req, res) {
-    db.query('select * from usuarios', (err, results) => {
+    db.query('select (nome, email, departamento, cargo) from usuarios', (err, results) => {
       if (err) {
         console.error('Erro ao executar a consulta:', err);
         res.status(500).json({ error: 'Erro ao buscar dados de usuário' });
