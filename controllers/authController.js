7a8c4696ff9e56ffd7fd3e537713f825a0221efa
Mateus Santos <54743403+mateusdata@users.noreply.github.com>
2023-10-09 08:15:03 -0300

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -17,7 +17,7 @@ function login(req, res) {
         }
         if (isMatch) {
           const token = jwt.sign({ id_token: 3 }, chaveSecreta, {
-            expiresIn: "20s", //duração em segundos
+            expiresIn: "10s", //duração em segundos
           });
           return res.send({ token, nome: results[0].nome });
         } else {
