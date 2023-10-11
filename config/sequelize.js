55c5e521853efa6236b78068bcfe3b9f1bd74648
mateusdata <mateuspele2015@gmail.com>
2023-10-11 04:02:59 -0300

    update sequelize

--- config/sequelize.js
+++ config/sequelize.js
@@ -4,7 +4,7 @@ const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USERNAME
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   dialect: 'mysql',
-  logging: false 
+ 
 });
 
 sequelize
--- models/UserModel.js
+++ models/UserModel.js
@@ -1,6 +1,17 @@
-const { Model, DataTypes } = require('sequelize');
+/*const { Sequelize, Model, DataTypes } = require('sequelize');
 require('dotenv').config();
-const sequelize = require("../config/sequelize")
+
+const sequelize = require("../config/sequelize")*/
+
+const { Sequelize, Model, DataTypes } = require('sequelize');
+require('dotenv').config();
+const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  dialect: 'mysql',
+ 
+});
+
 
 class Usuario extends Model {}
 
