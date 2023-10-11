7783d20009813bcafbae745a383dee1a1721e3e4
mateusdata <mateuspele2015@gmail.com>
2023-10-11 04:14:06 -0300

    update sequelize

--- index.js
+++ index.js
@@ -5,15 +5,15 @@ app.use(express.json());
 app.use(cors());
 const AuthRouter = require("./routes/auth");
 const ApiRouter = require("./routes/api");
-//const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3001;
 
-app.use("/", AuthRouter);
-app.use("/", ApiRouter);
+//app.use("/", AuthRouter);
+//app.use("/", ApiRouter);
 
 app.get("/", function (req, res) {
   res.send({projeto:"API do projeto de engenharia de software", rotas:""});
 });
 
-app.listen(3001, () => {
-  console.log("Servidor rodando na 3001a " + 3001);
+app.listen(port, () => {
+  console.log("Servidor rodando na porta " + port);
 });
