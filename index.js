3de865df262346ea4b2e4db5f7a082fa76e182c5
mateusdata <mateuspele2015@gmail.com>
2023-10-17 01:34:07 -0300

    added SRC

--- index.js
+++ index.js
@@ -3,15 +3,15 @@ const cors = require("cors");
 const app = express();
 app.use(express.json());
 app.use(cors());
-const AuthRouter = require("./routes/auth");
-const ApiRouter = require("./routes/api");
+const AuthRouter = require("./src/routes/auth");
+const ApiRouter = require("./src/routes/api");
 const port = process.env.PORT || 3001;
 
 app.use("/", AuthRouter);
 app.use("/", ApiRouter);
 
 app.get("/", function (req, res) {
-  res.send({projeto:"API do projeto de engenharia de software", rotas:"/auth/login  -  /cadastro -  /teste -  /lista-usuarios - /estalogado "});
+  res.send({ projeto: "API de Engenharia de Software", rotas: [ "/auth/login", "/cadastro", "/teste", "/lista-usuarios", "/estalogado" ] });
 });
 
 app.listen(port, () => {
