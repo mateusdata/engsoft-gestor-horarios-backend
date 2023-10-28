60780e9b2860c5d6636eaa10c7423c01176c48d1
Pedro <jpferreirafsa@gmail.com>
2023-10-28 20:00:04 -0300

    backend gerar código de recuperação

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ app.use(express.json());
 app.use(cors());
 const AuthRouter = require("./src/routes/auth");
 const ApiRouter = require("./src/routes/api");
+const sequelize = require("./src/config/sequelize");
 const port = process.env.PORT || 3001;
 
 app.use("/", AuthRouter);
@@ -13,7 +14,24 @@ app.use("/", ApiRouter);
 app.get("/", function (req, res) {
   res.send({ projeto: "API de Engenharia de Software", rotas: [ "/auth/login", "/cadastro", "/teste", "/lista-usuarios", "/estalogado" ] });
 });
-
+app.get("/recuperaremail", async function(req, res){
+  var email = "leroy@ifba.edu.br";
+  const codigo = Math.floor(Math.random() *999999);
+  try{
+    let response = await sequelize.query(`SELECT email FROM recuperarsenha WHERE email='${email}'`);
+    if(!response){
+      res.send("Usuário não cadastrado");
+    }
+    else{
+      res.send(response);
+      sequelize.query(`UPDATE recuperarsenha SET codigo='${codigo}'`);
+      
+    }
+  }
+  catch(error){
+    res.send("Usuário não cadastrado");
+  }
+});
 app.listen(port, () => {
   console.log("Servidor rodando na porta " + port);
 });
