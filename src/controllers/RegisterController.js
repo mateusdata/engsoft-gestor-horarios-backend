e96f93f15278591c2f492ec828d607e25ad77ca6
Mateus Santos <mateuspele2015@gmail.com>
2023-11-09 19:29:25 -0300

    added: new feature

--- src/controllers/RegisterController.js
+++ src/controllers/RegisterController.js
@@ -4,6 +4,51 @@ const UserModel = require("../models/UserModel");
 
 class RegisterController {
   async register(req, res) {
+    try {
+      let usersToRegister = [];
+
+      if (Array.isArray(req.body)) {
+        usersToRegister = req.body;
+      } else if (typeof req.body === 'object') {
+        usersToRegister.push(req.body);
+      } else {
+        return res.status(400).send({ error: 'Entrada inválida' });
+      }
+
+      const salt = await bcrypt.genSalt(10);
+      for (const user of usersToRegister) {
+        const hash = await bcrypt.hash(user.senha, salt);
+        await UserModel.create({
+          nome: user.nome,
+          email: user.email,
+          senha: hash,
+          matricula: user.matricula,
+          departamento: user.departamento,
+          cargo: user.cargo,
+          administrador: user.administrador,
+        });
+      }
+
+      res.status(200).send("Cadastro realizado com sucesso!");
+    } catch (error) {
+      res.status(500).send({ error: error.message });
+      console.error(error);
+    }
+  }
+}
+
+module.exports = new RegisterController();
+
+
+
+/*
+
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const UserModel = require("../models/UserModel");
+
+class RegisterController {
+  async register(req, res) {
     const { cargo, departamento, email, matricula, nome, senha,  administrador } = req.body;
     try {
       const salt = await bcrypt.genSalt(10);
@@ -26,3 +71,6 @@ class RegisterController {
 }
 
 module.exports = new RegisterController();
+
+
+*/
