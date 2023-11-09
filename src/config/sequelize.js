c69789b08996ca9de8e43eae7cd4ab74ebed75ef
Mateus Santos <mateuspele2015@gmail.com>
2023-11-09 18:52:23 -0300

    added: suport for psql

--- src/config/sequelize.js
+++ src/config/sequelize.js
@@ -1,14 +1,23 @@
 const { Sequelize } = require('sequelize');
 const mysql2 = require('mysql2');
+const pg = require('pg');
 
-const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+
+/*const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   dialect: 'mysql',
   dialectModule: mysql2, // Usar o pacote mysql2 se nao usar isso nao da pra subir a api na vercel
   //essa foi a forma que eu achei pra corrigir o bug
 });
-
+*/
+const sequelize = new Sequelize(
+  process.env.psql,
+   {
+  dialect: 'postgres',
+  dialectModule: pg, // Usar o pacote mysql2 se nao usar isso nao da pra subir a api na vercel
+  //essa foi a forma que eu achei pra corrigir o bug
+});
 sequelize
   .authenticate()
   .then(() => {
