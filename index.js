c2c149fcf642f2cf6b68435de188f54c15cf6ea5
vinicius-0402 <viniciusbr2029@gmail.com>
2023-11-24 08:32:41 -0300

    Modelos dos Semestres

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 3001;
 const ResetController = require("./src/controllers/ResetController");
 const TesteController = require("./src/controllers/TesteController");
 const sequelize = require("./src/config/sequelize");
-const SchedulesController = require("./src/controllers/SchedulesController");
+const TeachersController = require("./src/controllers/TeachersController");
 app.use("/", AuthRouter);
 app.use("/", ApiRouter);
 app.get("/", function (req, res) {
@@ -20,7 +20,7 @@ app.post("/recuperaremail", ResetController.recuperarEmail);
 app.post("/validarcodigo", ResetController.validarCodigo);
 app.post("/resetarsenha", ResetController.resetarSenha);
 app.get("/rotateste", TesteController.testarBanco);
-app.get("/teacher_list", SchedulesController.show_teacher);
+app.get("/teacher_list", TeachersController.show_teacher);
 app.listen(port, () => {
    console.log("Servidor rodando na porta " + port);
 });
