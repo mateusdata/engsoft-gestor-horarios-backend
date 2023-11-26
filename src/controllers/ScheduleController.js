5c8d95924b7dac15b460d37612e63720b62fa176
Pedro <jpferreirafsa@gmail.com>
2023-11-26 12:12:28 -0300

    edição do ScheduleController para edição no frontend

--- src/controllers/ScheduleController.js
+++ src/controllers/ScheduleController.js
@@ -3,7 +3,8 @@ const sequelize = require("../config/sequelize");
 
 class ScheduleController{
     async mostrarHorario (req,res){
-        const {semestre} = req.body;
+        //const {semestre} = req.body;
+        const semestre = "prim_sem";
         const query = await sequelize.query(`select * from ${semestre}`);
         res.send(query[0]);
     }
