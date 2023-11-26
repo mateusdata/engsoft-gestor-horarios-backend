cab8f6fc391e549c0c0e11ace4e0a387e01f33b1
Pedro <jpferreirafsa@gmail.com>
2023-11-26 17:09:35 -0300

    menores alterações

--- src/controllers/ScheduleController.js
+++ src/controllers/ScheduleController.js
@@ -3,8 +3,8 @@ const sequelize = require("../config/sequelize");
 
 class ScheduleController{
     async mostrarHorario (req,res){
-        const {semestre} = req.body;
-
+        //const {semestre} = req.body;
+        const semestre = "prim_sem";
         var horarios = ["prim_hor", "segu_hor", "terc_hor", "quar_hor", "quin_hor", "sext_hor"];
         var materias = ["prim_hor_materia", "segu_hor_materia", "terc_hor_materia", "quar_hor_materia", "quin_hor_materia", "sext_hor_materia"]
         try{
