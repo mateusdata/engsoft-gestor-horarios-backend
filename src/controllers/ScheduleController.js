1fb3c616ac8f7377323e267956212e3c34667e01
Pedro <jpferreirafsa@gmail.com>
2023-11-30 20:26:41 -0300

    alterações nos hoários para consultar nova tabela unificada

--- src/controllers/ScheduleController.js
+++ src/controllers/ScheduleController.js
@@ -4,11 +4,12 @@ const sequelize = require("../config/sequelize");
 class ScheduleController{
     async mostrarHorario (req,res){
         //const {semestre} = req.body;
-        const semestre = "prim_sem";
+        const semestre = "terceiro";
+        const modalidade = "bacharelado_si";
         var horarios = ["prim_hor", "segu_hor", "terc_hor", "quar_hor", "quin_hor", "sext_hor"];
         var materias = ["prim_hor_materia", "segu_hor_materia", "terc_hor_materia", "quar_hor_materia", "quin_hor_materia", "sext_hor_materia"]
         try{
-            const query = await sequelize.query(`select prim_hor, prim_hor_materia, segu_hor, segu_hor_materia, terc_hor, terc_hor_materia, quar_hor, quar_hor_materia, quin_hor, quin_hor_materia, sext_hor, sext_hor_materia from ${semestre}`);
+            const query = await sequelize.query(`select prim_hor, prim_hor_materia, segu_hor, segu_hor_materia, terc_hor, terc_hor_materia, quar_hor, quar_hor_materia, quin_hor, quin_hor_materia, sext_hor, sext_hor_materia from ${modalidade} where id='${semestre}'`);
             const usuarios = await sequelize.query(`select matricula, nome from usuarios`);
             const disciplinas = await sequelize.query(`select cod,nome from disciplinas`);
             for(var c=0; c<query[0].length; c++){
