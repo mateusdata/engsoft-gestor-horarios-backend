9a663752680fa566402064a1c76b6b0796254ccd
vinicius-0402 <viniciusbr2029@gmail.com>
2023-12-10 20:26:00 -0300

    Atualização dos Horários

--- src/controllers/ScheduleController.js
+++ src/controllers/ScheduleController.js
@@ -3,8 +3,8 @@ const sequelize = require("../config/sequelize");
 
 class ScheduleController{
     async mostrarHorario (req,res){
-        //const {semestre} = req.body;
-        const semestre = "primeiro";
+        const {semestre} = req.body;
+        //const semestre = "primeiro";
         const modalidade = "bacharelado_si";
         const horarios = ["prim_hor", "segu_hor", "terc_hor", "quar_hor", "quin_hor", "sext_hor"];
         const materias = ["prim_hor_materia", "segu_hor_materia", "terc_hor_materia", "quar_hor_materia", "quin_hor_materia", "sext_hor_materia"]
@@ -32,6 +32,32 @@ class ScheduleController{
             res.send("erro fatal");
         }
 
+    }
+
+    async currentHourlyData(req,res){
+        try{
+            const query = await sequelize.query(`select prim_hor, prim_hor_materia, segu_hor, segu_hor_materia, terc_hor, terc_hor_materia, quar_hor, quar_hor_materia, quin_hor, quin_hor_materia, sext_hor, sext_hor_materia from bacharelado_si where id=primeiro`);
+            res.send(query[0]);
+        }
+        catch{
+            res.send("Erro.");
+        }
+    }
+
+    async updateSchedules(req,res){
+        const {prim_hor, prim_hor_materia, segu_hor, segu_hor_materia, terc_hor, terc_hor_materia, quar_hor, quar_hor_materia, quin_hor, quin_hor_materia, sext_hor, sext_hor_materia} = req.body;
+
+        try{
+            const query =  await sequelize.query(`UPDATE bacharelado_si SET prim_hor='${prim_hor}', prim_hor_materia='${prim_hor_materia}', segu_hor='${segu_hor}', segu_hor_materia='${segu_hor_materia}', terc_hor='${terc_hor}', terc_hor_materia='${terc_hor_materia}', quar_hor='${quar_hor}', quar_hor_materia='${quar_hor_materia}', quin_hor='${quin_hor}', quin_hor_materia='${quin_hor_materia}', sext_hor='${sext_hor}', sext_hor_materia='${sext_hor_materia}' where id=primeiro`);
+            
+            if(query){
+                return  res.send("Dados Atualizados com Sucesso !");
+               }
+               return  res.send("Erro ao Atualizar o Horário.");
+            }
+            catch(error){
+                res.status(500).send({ error: error, mensage:"Erro 500."});
+            }
     }
 }
 
